a81d41341317f10a543be94260c6f90fb91247a6
Muhodari Sage <sagemuho@gmail.com>
2021-07-27 14:53:30 +0200

    restarted

--- restframework/views.py
+++ restframework/views.py
@@ -3,6 +3,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
+# from .serializers import StudentSerializer
+# from .models import Student
+#
+
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
         data = {
@@ -11,3 +15,11 @@ class TestView(APIView):
         }
 
         return Response(data)
+
+    # def post(self, request, *args, **kwargs):
+    #     serializer = StudentSerializer(data=request.data)
+    #
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors)

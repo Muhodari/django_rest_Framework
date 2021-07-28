5ea351982d2f86f66cb03bfd3c07c31a9ed9d6ad
Muhodari Sage <sagemuho@gmail.com>
2021-07-28 14:51:50 +0200

    finally done with dango_rest_frmaework and added authentication

--- restframework/views.py
+++ restframework/views.py
@@ -3,12 +3,18 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from app.serializers import StudentSerializer
 from app.models import Student
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 
 class TestView(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request, *args, **kwargs):
         qs = Student.objects.all()
-        serializer = StudentSerializer(qs, many=True)
+        student1 = qs.first()
+        # serializer = StudentSerializer(qs, many=True)
+        serializer = StudentSerializer(student1)
         return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
